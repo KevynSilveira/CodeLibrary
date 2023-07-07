4a339d3613f26a90df9a0deec985db95d3e82fc5
Kevyn <kevynsilveira0610@gmail.com>
2023-07-06 23:10:15 -0300

    Adicionado comentarios e organizado o codigo para ficar mais coerente

--- Interface.py
+++ Interface.py
@@ -1,5 +1,4 @@
 import customtkinter
-import tkinter as tk
 
 def Cria_frame_principal():
     #Cria a janela principal do codigo.
@@ -8,30 +7,34 @@ def Cria_frame_principal():
     FPrincipal.title("Biblioteca de comandos")
     FPrincipal.resizable(False, False)
 
+    #Frame principal
     FLinguagens = customtkinter.CTkFrame(master=FPrincipal, width=200, height=570, corner_radius=10)
     FLinguagens.place(x=10, y=10)
 
-    FConteudo = customtkinter.CTkFrame(master=FPrincipal, width=770, height=480, corner_radius=10)
-    FConteudo.place(x=220, y=100)
+    #Campo pesquisa
+    CPesquisa = customtkinter.CTkEntry(master=FPrincipal, width=540, height=30, corner_radius=10)
+    CPesquisa.place(x=240, y=10)
+    text = CPesquisa.get()
 
+    #Opcoes da combobox
     opcoes_filtro = ["Linguagem", "Codigo", "Conteudo"]
+    #Botao combobox filtro
     CFiltro = customtkinter.CTkComboBox(master=FPrincipal, values=opcoes_filtro, width=200, height=30)
     CFiltro.place(x=790, y=10)
 
-    CPesquisa = customtkinter.CTkEntry(master=FPrincipal, width=540, height=30, corner_radius=10)
-    CPesquisa.place(x=240, y=10)
+    #Frame conteudo
+    FConteudo = customtkinter.CTkFrame(master=FPrincipal, width=770, height=480, corner_radius=10)
+    FConteudo.place(x=220, y=100)
 
-    text = CPesquisa.get()
-
-    label_selecione_layout = customtkinter.CTkLabel(master=FConteudo, text="Selecione o layout")
-    label_selecione_layout.place(x=60, y=900)
-
-    BExportar = customtkinter.CTkButton(master=FPrincipal, text="Exportar")
-    BExportar.place(x=840, y=540)
-
+    #Scroll bar do frame conteudo
     scrollbar = customtkinter.CTkScrollbar(FConteudo)
     scrollbar.place(x=750, y=10)
 
+    #Botao exportar
+    BExportar = customtkinter.CTkButton(master=FPrincipal, text="Exportar")
+    BExportar.place(x=840, y=540)
+
+    #Executa o Frame principal
     FPrincipal.mainloop()
 
 if __name__ == "__main__":
