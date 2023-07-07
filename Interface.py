6da463706042f356c59e07c8918de464c357209b
Kevyn <kevynsilveira0610@gmail.com>
2023-07-06 22:38:14 -0300

    Feito a criacao dos principais frames

--- Interface.py
+++ Interface.py
@@ -1,14 +1,35 @@
 import customtkinter
+import tkinter as tk
 
 def Cria_frame_principal():
     #Cria a janela principal do codigo.
     FPrincipal = customtkinter.CTk()
-    FPrincipal.geometry("1000x700")
+    FPrincipal.geometry("1000x590")
     FPrincipal.title("Biblioteca de comandos")
     FPrincipal.resizable(False, False)
+
+    FLinguagens = customtkinter.CTkFrame(master=FPrincipal, width=200, height=570, corner_radius=10)
+    FLinguagens.place(x=10, y=10)
+
+    FConteudo = customtkinter.CTkFrame(master=FPrincipal, width=770, height=480, corner_radius=10)
+    FConteudo.place(x=220, y=100)
+
+    opcoes_layout = ["SQL", "Python", "GIT"]
+    CFiltro = customtkinter.CTkComboBox(master=FPrincipal, values=opcoes_layout, width=200, height=30)
+    CFiltro.place(x=790, y=10)
+
+    CPesquisa = customtkinter.CTkEntry(master=FPrincipal, width=540, height=30, corner_radius=10)
+    CPesquisa.place(x=240, y=10)
+
+    text = CPesquisa.get()
+
+    label_selecione_layout = customtkinter.CTkLabel(master=FConteudo, text="Selecione o layout")
+    label_selecione_layout.place(x=60, y=900)
+
+    scrollbar = customtkinter.CTkScrollbar(FConteudo)
+    scrollbar.place(x=750, y=10)
+
     FPrincipal.mainloop()
-
-
 
 if __name__ == "__main__":
     Cria_frame_principal()
