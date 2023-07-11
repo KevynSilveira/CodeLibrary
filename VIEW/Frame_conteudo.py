54006c7e65b847cdc00a4f44d52c25d0a0064426
Kevyn <kevynsilveira0610@gmail.com>
2023-07-11 20:56:28 -0300

    Feito mais alguns teste, colocando um botao para exibir a descricao completa

--- VIEW/Frame_conteudo.py
+++ VIEW/Frame_conteudo.py
@@ -13,24 +13,25 @@ def criar_frame_conteudo(linguagem, comando, descricao):
     FPrincipal.title("Biblioteca de comandos")
     FPrincipal.resizable(False, False)
 
-    FConteudo = customtkinter.CTkFrame(master=FPrincipal, width=500, height=60)
+    FConteudo = customtkinter.CTkFrame(master=FPrincipal, width=500, height=45)
     FConteudo.place(x=250, y=10)
 
-    FLinguagem = customtkinter.CTkFrame(master=FConteudo, width=50, height=60)
+    FLinguagem = customtkinter.CTkFrame(master=FConteudo, width=50, height=45)
     FLinguagem.place(x=0, y=0)
     Llinguagem = customtkinter.CTkLabel(master=FLinguagem, text=linguagem)
-    Llinguagem.place(x=0, y=0)
+    Llinguagem.place(relx=0.5, rely=0.5, anchor="center")
 
-    FComando = customtkinter.CTkFrame(master=FConteudo, width=100, height=60)
+    FComando = customtkinter.CTkFrame(master=FConteudo, width=100, height=45)
     FComando.place(x=50, y=0)
     LComando = customtkinter.CTkLabel(master=FComando, text=comando)
-    LComando.place(x=0, y=0)
+    LComando.place(relx=0.5, rely=0.5, anchor="center")
 
-    FDescricao = customtkinter.CTkFrame(master=FConteudo, width=350, height=60)
+    FDescricao = customtkinter.CTkFrame(master=FConteudo, width=350, height=45)
     FDescricao.place(x=150, y=0)
     LDescricao = customtkinter.CTkLabel(master=FDescricao, text=descricao)
-    LDescricao.place(x=0, y=0)
-
+    LDescricao.place(relx=0.5, rely=0.5, anchor="center")
+    BDescricao = customtkinter.CTkButton(master=FDescricao, width=10, height=10, text="", corner_radius=2)
+    BDescricao.place(x=340, y=35)
 
     FPrincipal.mainloop()
 
