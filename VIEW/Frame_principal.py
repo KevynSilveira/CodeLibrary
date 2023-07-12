4ddf4c0234c3b6f972f51130626d38c618e38ea8
Kevyn <kevynsilveira0610@gmail.com>
2023-07-11 21:52:24 -0300

    Integrado com o frame principal, onde rodara a interface, ainda esta com erro ao chamar a funcao

--- VIEW/Frame_principal.py
+++ VIEW/Frame_principal.py
@@ -1,6 +1,6 @@
 import customtkinter
 from Funcoes_frame import tema
-
+import textwrap
 
 def criar_frame_principal():
     # Cria a janela principal do código.
@@ -72,6 +72,67 @@ def criar_frame_principal():
                                               command=cadastrar)
         BCadcomando.place(x=260, y=290)
 
+    def criar_frame_conteudo(linguagem, comando, descricao):
+        # Definição das variáveis
+        linguagem = linguagem
+        comando = comando
+        descricao = descricao
+
+        # Tamanho máximo de caracteres para a descrição
+        max_caracteres = 30
+
+        def mostrar_descricao():
+            def voltar_descricao():
+                MDescricao.destroy()
+                BVoltar.destroy()
+
+            MDescricao = customtkinter.CTkTextbox(master=FConteudo, width=740, height=100)
+            MDescricao.insert(customtkinter.END, descricao)
+            MDescricao.bind("<Key>", lambda e: "break")  # Desativa a entrada do teclado
+            MDescricao.place(x=10, y=10)
+
+            BVoltar = customtkinter.CTkButton(master=FConteudo, width=10, height=10, text="", corner_radius=2,
+                                              command=voltar_descricao)
+            BVoltar.place(x=740, y=10)
+
+        y_pos = 10  # Posição vertical inicial
+
+        for _ in range(3):  # Número de frames a serem criados
+            # Cria o frame para o conteúdo
+            FBlocoConteudo = customtkinter.CTkFrame(master=FConteudo, width=740, height=45)
+            FBlocoConteudo.place(x=10, y=y_pos)
+
+            # Cria o frame para exibir a linguagem
+            FLinguagem = customtkinter.CTkFrame(master=FBlocoConteudo, width=100, height=45)
+            FLinguagem.place(x=0, y=0)
+            Llinguagem = customtkinter.CTkLabel(master=FLinguagem, text=linguagem)
+            Llinguagem.place(relx=0.5, rely=0.5, anchor="center")
+
+            # Cria o frame para exibir o comando
+            FComando = customtkinter.CTkFrame(master=FBlocoConteudo, width=200, height=45)
+            FComando.place(x=100, y=0)
+            LComando = customtkinter.CTkLabel(master=FComando, text=comando)
+            LComando.place(relx=0.5, rely=0.5, anchor="center")
+
+            # Cria o frame para exibir a descrição
+            FDescricao = customtkinter.CTkFrame(master=FBlocoConteudo, width=440, height=45)
+            FDescricao.place(x=300, y=0)
+            BDescricao = customtkinter.CTkButton(master=FDescricao, width=10, height=10, text="", corner_radius=2,
+                                                 command=mostrar_descricao)
+            BDescricao.place(x=430, y=35)
+
+            # Verifica se o texto excede o limite e adiciona os três pontos se necessário
+            if len(descricao) > max_caracteres:
+                texto_exibido = textwrap.shorten(descricao, width=max_caracteres - 3, placeholder="...")
+            else:
+                texto_exibido = descricao
+            LDescricao = customtkinter.CTkLabel(master=FDescricao, text=texto_exibido)
+            LDescricao.place(relx=0.5, rely=0.5, anchor="center")
+
+            y_pos += 55  # Atualiza a posição vertical para o próximo frame
+
+        # Inicia o loop principal da janela
+
     # Frame principal
     FLinguagens = customtkinter.CTkFrame(master=FPrincipal, width=200, height=570, corner_radius=10)
     FLinguagens.place(x=10, y=10)
@@ -125,9 +186,26 @@ def criar_frame_principal():
     BCadastrar = customtkinter.CTkButton(master=FPrincipal, text="Cadastrar", command=cadastro_comando)
     BCadastrar.place(x=670, y=540)
 
+    git = "Git"
+    sql = "SQL"
+    python = "Python"
+
+    comandoGit = "git add . "
+    comandoSQL = "Select * From"
+    comandoPython = "import"
+
+    descricaoGit = "Adiciona arquivos no stage"
+    descricaoSql = "Seleciona arquivos"
+    descricaoPython = "Importa bibliotecasjuyEJtrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
+
+    criar_frame_conteudo(git, comandoGit, descricaoGit)
+    criar_frame_conteudo(sql, comandoSQL, descricaoSql)
+    criar_frame_conteudo(python, comandoPython, descricaoPython)
+
     # Executa o Frame principal
     FPrincipal.mainloop()
 
 
 if __name__ == "__main__":
     criar_frame_principal()
+
